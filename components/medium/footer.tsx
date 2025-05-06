bc99ec906e22cacc0e3f40838ff5b018389e0d9c
qubitnumber <qubitnumber@gmail.com>
2025-05-06 11:45:58 -0400

    .

--- components/medium/footer.tsx
+++ components/medium/footer.tsx
@@ -5,17 +5,6 @@ export default function Footer() {
     <footer className='py-4'>
       <div className='container flex flex-col items-center justify-between gap-x-3 gap-y-1 text-center text-sm text-muted-foreground sm:flex-row'>
         <p>Bridge AI &copy;{new Date().getFullYear()}. All rights reserved.</p>
-        <p className='text-xs'>
-          Developed by{' '}
-          <Link
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-primary transition-colors hover:text-accent-foreground'
-            href='https://www.hamedbahram.io/'
-          >
-            Studio H
-          </Link>
-        </p>
       </div>
     </footer>
   )
